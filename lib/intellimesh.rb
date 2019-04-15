1b5cf467c4ebedadf271a85025dc2aeec462687e
Dan Thomas <dan@ideacrew.com>
2019-04-15 18:01:19 -0400

    Add loggable and passing spec

--- lib/intellimesh.rb
+++ lib/intellimesh.rb
@@ -1,12 +1,17 @@
 require "active_support"
+
 require "intellimesh/version"
-require "intellimesh/config"
+# require "intellimesh/config"
 require "intellimesh/publishers"
 require "intellimesh/subscribers"
+require "intellimesh/loggable"
+
 
 # require "intellimesh/amqp/worker_specification"
 
 module Intellimesh
+  extend Loggable
+
   class Error < StandardError; end
   # Your code goes here...
 end
