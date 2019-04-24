be125acfe5d5953dcd5a851c7a2bd43907b68bca
Trey Evans <tevans@Just-Plain-Awful.local>
2019-04-24 17:09:28 -0400

    Remove custom uri for unified uri.

--- lib/intellimesh/uri.rb
+++ lib/intellimesh/uri.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require File.join(File.dirname(__FILE__), "uri", "amqp")
-require File.join(File.dirname(__FILE__), "uri", "amqpreply")
 require File.join(File.dirname(__FILE__), "uri", "sftp")
 # Disabling until specs run again.
 # require "uri/wks"
